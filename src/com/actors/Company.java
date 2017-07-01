183a8be68b55c097d854e773fa00d7a6950583c4
Arjun <arjun2504@gmail.com>
2017-07-01 21:31:20 +0530

    Initial Commit

---
+++ src/com/actors/Company.java
@@ -0,0 +1,38 @@
+package com.actors;
+
+public class Company {
+	int id, totalShares, availableShares, currentPrice;
+	String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getTotalShares() {
+		return totalShares;
+	}
+	public void setTotalShares(int totalShares) {
+		this.totalShares = totalShares;
+	}
+	public int getAvailableShares() {
+		return availableShares;
+	}
+	public void setAvailableShares(int availableShares) {
+		this.availableShares = availableShares;
+	}
+	public int getCurrentPrice() {
+		return currentPrice;
+	}
+	public void setCurrentPrice(int currentPrice) {
+		this.currentPrice = currentPrice;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/com/actors/User.java
@@ -0,0 +1,39 @@
+package com.actors;
+
+public class User {
+	String id, 	location, phone, email;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
